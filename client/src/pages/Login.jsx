e4e2d3cddc88cf22207908e71dae1e5209c796b6
Mobe <mo@be.org>
2023-11-01 03:16:32 +0100

    - Login.jsx:   - Added new imports for useState, useEffect, useNavigate, Link, styled-components,     Logo, ToastContainer, toast, axios, and loginRoute.   - Added new state variables for email and password.   - Added validation functions for email and password.   - Added functions to handle form submission and input changes.
    - userController.js:
      - Renamed the file from userControllers.js to userController.js.
      - Added a new function for user login that includes validation for email and password,
        a check for the existence of the user, and a comparison of the password
        with the hashed password in the database.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,193 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useNavigate, Link} from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg"
+import {ToastContainer, toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import axios from "axios";
+import {loginRoute} from "../utils/APIRoutes";
 
 function Login() {
-    return (<div>Login</div>);
+
+    const navigate = useNavigate()
+
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 3000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    const validate = (value, rule, message, errors) => {
+        if (!rule(value)) {
+            toast.error(message, toastOptions);
+            errors.push(message);
+        }
+    }
+
+    const handleValidation = () => {
+        const {password, email} = values;
+        let errors = [];
+
+        validate(password, val => val.length > 0, "Password is required!", errors);
+        validate(email, val => val.length > 0, "Email is required!", errors);
+
+        return errors.length === 0;
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (handleValidation()) {
+            const {email, password} = values;
+
+            try {
+                const {data} = await axios.post(loginRoute, {
+                    email,
+                    password,
+                });
+
+                if (data.status === false) {
+                    toast.error(data.msg, toastOptions);
+                } else {
+                    localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+                    navigate("/")
+                }
+
+            } catch (error) {
+                console.error("An error occurred while trying to send POST request: ", error);
+            }
+        } else {
+            console.error("Validation failed");
+        }
+
+    };
+
+    const handleChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value})
+    };
+
+    return (
+        <>
+            <FormContainer>
+                <form autoComplete="on"
+                      onSubmit={event => handleSubmit(event)}>
+                    <div className="brand">
+                        <img src={Logo} alt="Logo"/>
+                        <h1>snappy</h1>
+                    </div>
+                    <input type="email"
+                           placeholder="E-Mail"
+                           name="email"
+                           autoComplete="on"
+                           max="50"
+                           onChange={event => handleChange(event)}/>
+                    <input type="password"
+                           placeholder="Password"
+                           name="password"
+                           min="8"
+                           onChange={event => handleChange(event)}/>
+                    <button type="submit">Login User</button>
+                    <span>
+                        Don't have an account?
+                        <Link to="/register">Register</Link>
+                    </span>
+                </form>
+            </FormContainer>
+            <ToastContainer/>
+        </>
+    );
 }
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color: #131324;
+
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+
+    img {
+      height: 5rem;
+    }
+
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-out;
+
+      &:hover {
+        background-color: #4e0eff;
+      }
+    }
+
+    span {
+      color: white;
+      text-transform: uppercase;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      gap: 1rem;
+
+      a {
+        color: #4e0eff;
+        text-transform: none;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  }
+`;
 
 export default Login;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,4 +34,30 @@ module.exports.register = async (req, res, next) => {
     } catch (ex) {
         next(ex);
     }
+}
+
+module.exports.login = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email})
+
+        if (!user) {
+            console.log("WTF? " + email)
+            return res.json({msg: "Incorrect Username or Password", status: false});
+        }
+        console.log("WTF! " + email)
+
+        const hasValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!hasValidPassword) {
+            return res.json({msg: "Incorrect Username or Password", status: false});
+        }
+
+        delete user.password;
+        return res.json({ status: true, user });
+
+    } catch (ex) {
+        next(ex);
+    }
 };
