4ec62911ccd4f97c2650992e424ff156e2ee309c
Mobe <mo@be.org>
2023-11-01 23:17:23 +0100

    - userRoutes.js: A new function called 'updateAvatar' was added to the 'userController' object.   This change adds functionality to the userController object by allowing users to update their avatar. - userRoutes.js: The 'updateAvatar' function was added as a new route to the router object.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const {register, login} = require("../controllers/userController");
+const {register, login, updateAvatar} = require("../controllers/userController");
 const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/update-avatar/:id", updateAvatar);
 
 module.exports = router;
