ccef3442058ed43ec4d5490629fbb0eb3ac2d7f6
Mobe <mo@be.org>
2023-11-01 23:12:35 +0100

    - Register.jsx: The value of the localStorage.getItem() function has been changed   from "chat-app-user" to "process.env.REACT_APP_LOCALHOST_KEY".   This change updates the key used to retrieve the user data from the local storage. - Register.jsx: The value of the localStorage.setItem() function has been changed   from "chat-app-user" to "process.env.REACT_APP_LOCALHOST_KEY".   This change updates the key used to store the user data in the local storage.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -20,7 +20,7 @@ function Register() {
     };
 
     useEffect(() => {
-        if (localStorage.getItem("chat-app-user")) {
+        if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
             navigate("/");
         }
     }, []);
@@ -67,7 +67,7 @@ function Register() {
                 if (data.status === false) {
                     toast.error(data.msg, toastOptions);
                 } else {
-                    localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+                    localStorage.setItem(process.env.REACT_APP_LOCALHOST_KEY, JSON.stringify(data.user));
                 }
                 navigate("/")
 
