3ef28937072545c6bc4b53ea32e27abdebb6c137
Mobe <mo@be.org>
2023-11-04 15:26:27 +0100

    - Welcome.jsx:   new file has been added to the "client/src/components" directory.

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif"
+
+export default function Welcome({currentUser}) {
+  return (
+      <>
+
+      </>
+  )
+}
