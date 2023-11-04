6f0d30ecdae17a48ca647ebd9ba9aa82232ceb6d
Mobe <mo@be.org>
2023-11-04 16:03:51 +0100

    - Welcome.jsx:   The return statement of the Welcome component was modified to include a Container component,   an image of a robot, a welcome message with the user's name, and a message prompting the user to choose a chat.   This change was made to improve the user experience by providing   a personalized welcome message and clear instructions on how to proceed.

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Robot from "../assets/robot.gif"
 
-export default function Welcome({currentUser}) {
+export default function Welcome() {
+
+  const [userName, setUserName] = useState("");
+
+  useEffect(
+      () => {
+        setUserName(
+            JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)).username
+        );
+      }
+  );
+
   return (
       <>
-
+        <Container>
+          <img src={Robot} alt="robot"/>
+          <h1>Welcome, <span>{userName?userName : 'Guest'}!</span></h1>
+          <h3>Hey there! To get started with messaging, please choose a chat</h3>
+        </Container>
       </>
-  )
-}
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+
+  img {
+    height: 20rem;
+  }
+
+  span {
+    color: #4e0eff;
+  }
+`;
